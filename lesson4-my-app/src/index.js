d6414fdbcceb7bc00cde7e34c005504e2862eeaa
Murat Bukarbaev <murat.bukarbaev@gmail.com>
2024-08-21 08:49:15 +0600

    lesson4-my-app

--- lesson4-my-app/src/index.js
+++ lesson4-my-app/src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { rootReducer } from './redux';
-import { thunk } from 'redux-thunk';
-import logger from 'redux-logger';
 
-const myStore = createStore(rootReducer, applyMiddleware(thunk, logger))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={myStore}>
+    <Provider store={}>
         <App />
     </Provider>
 );
